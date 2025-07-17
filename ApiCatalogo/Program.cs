f49b22180a36797b484e145414da682b69299ef8
Filipe Gabriel <filipegabriel11@hotmail.com>
2025-07-17 18:18:29 -0300

    Endpoints Put e Delete criados

--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -36,6 +36,36 @@ app.MapPost("/categorias", async (AppDbContext context, Categoria categoria) =>
     return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
 });
 
+app.MapPut("/categorias/{id:int}", async (AppDbContext context, Categoria categoria, int id) =>
+{
+    if (categoria.CategoriaId != id)
+        return Results.BadRequest("ID inválido.");
+
+    var categoriaDb = await context.Categorias.FindAsync(id);
+
+    if (categoriaDb is null)
+        return Results.NotFound($"Categoria com ID {id} não encontrada.");
+
+    categoriaDb.Nome = categoria.Nome;
+    categoriaDb.Descricao = categoria.Descricao;
+
+    await context.SaveChangesAsync();
+    return Results.Ok(categoriaDb);
+});
+
+app.MapDelete("/categorias/{id:int}", async (AppDbContext context, int id) =>
+{
+    var categoria = await context.Categorias.FindAsync(id);
+
+    if (categoria is null)
+        return Results.NotFound($"Categoria com ID {id} não encontrada.");
+
+    context.Categorias.Remove(categoria);
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
--- ApiCatalogo/obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
+++ ApiCatalogo/obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f13370309bb2a5ebd6cf0f382b04a795548ef146")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb8c24d121772a51e7bb6b6b857a09c8191750b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
